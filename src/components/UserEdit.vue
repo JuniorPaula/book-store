f53aa8d10b9f696b30e7bd21c35223deae6ee710
Junior Paula <luke.junnior@icloud.com>
2024-03-16 09:55:43 -0300

    Implements user edit component

--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -3,6 +3,57 @@
     <div class="row">
       <div class="col">
         <h1 class="mt-3">User Edit</h1>
+        <hr />
+
+        <FormTag @userEditEvent="submitHandler" name="userform" event="userEditEvent">
+          <TextInput
+            v-model="user.first_name"
+            type="text"
+            :required="true"
+            label="First Name"
+            :value="user.first_name"
+            name="first_name"
+          />
+
+          <TextInput
+            v-model="user.last_name"
+            type="text"
+            :required="true"
+            label="Last Name"
+            :value="user.last_name"
+            name="last_name"
+          />
+
+          <TextInput
+            v-model="user.email"
+            type="email"
+            :required="true"
+            label="Email"
+            :value="user.email"
+            name="email"
+          />
+
+          <TextInput
+            v-if="user.id === 0"
+            v-model="user.password"
+            type="password"
+            :required="true"
+            label="Password"
+            :value="user.password"
+            name="password"
+          />
+
+          <hr>
+          <div class="float-start">
+            <input type="submit" class="btn btn-primary me-2" value="Save">
+            <router-link to="/admin/users" class="btn btn-secondary">Cancel</router-link>
+          </div>
+          <div class="float-end">
+            <a v-if="(this.$route.params.userId > 0) && (parseInt(String(this.$route.params.userId), 10)) !== store.user.id"
+              class="btn btn-danger" href="javascript:void(0);" @click="confirmDelete(this.user.id)">Delete</a>
+          </div>
+          <div class="clearfix"></div>
+        </FormTag>
       </div>
     </div>
   </div>
@@ -10,10 +61,76 @@
 
 <script>
 import { Security } from './security.js'
+import { store } from './store.js'
+import FormTag from './forms/FormTag.vue'
+import TextInput from './forms/TextInput.vue'
+import notie from 'notie'
+// import router from '../router/index.js'
 
 export default {
   beforeMount() {
     Security.requireToken()
+
+    if (parseInt(String(this.$route.params['userId']), 10) > 0) {
+      // editing the existing user
+      // TODO: Get the user data from the database
+    }
+  },
+  data() {
+    return {
+      user: {
+        id: 0,
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+      },
+      store
+    }
+  },
+  components: {
+    FormTag,
+    TextInput
+  },
+  methods: {
+    submitHandler() {
+      const payload = {
+        id: parseInt(String(this.$route.params['userId']), 10),
+        first_name: this.user.first_name,
+        last_name: this.user.last_name,
+        email: this.user.email,
+        password: this.user.password,
+      }
+
+      fetch(`${process.env.VUE_APP_API_URL}/admin/users/save`, Security.requestOptions(payload))
+        .then(response => response.json())
+        .then(data => {
+          if (data.error) {
+            notie.alert({
+              type: 'error',
+              text: data.message,
+              time: 3
+            })
+          } else {
+            notie.alert({
+              type: 'success',
+              text: 'User saved successfully',
+              time: 3
+            })
+        }
+      })
+      .catch(() => {
+        notie.alert({
+          type: 'error',
+          text: 'An error occurred while saving the user',
+          time: 3
+        })
+      })
+    },
+    confirmDelete() {
+      // 
+    }
   }
 }
 </script>
