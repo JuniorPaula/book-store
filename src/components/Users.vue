dafde318b1e6c1d9d7d8a1458d8f586be7392b57
Junior Paula <luke.junnior@icloud.com>
2024-03-16 12:16:29 -0300

    Update the log user out

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -11,6 +11,7 @@
           <tr>
             <th>User</th>
             <th>Email</th>
+            <th>Active</th>
             <th>Status</th>
           </tr>
         </thead>
@@ -20,6 +21,12 @@
               <router-link :to="`/admin/users/${u.id}`">{{ u.first_name }} {{ u.last_name }}</router-link>
             </td>
             <td>{{ u.email }}</td>
+            <td v-if="u.active === 0">
+              <span class="badge bg-danger">Inactive</span>
+            </td> 
+            <td v-else>
+              <span class="badge bg-success">Active</span>
+            </td> 
             <td v-if="u.token.id > 0">
               <span class="badge bg-success" @click="logUserOut(u.id)">Logged in</span>
             </td>
